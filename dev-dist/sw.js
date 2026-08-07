1546393d36018ff03ea6ba42ec68878d8f551808
Charan-coder-bit <hncharan328@gmail.com>
2026-08-07 16:51:01 +0530

    Fix data sync bug, fix old notes upload fallback, add Settings page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.bituacdtqv"
+    "revision": "0.pd2n9jipn2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
